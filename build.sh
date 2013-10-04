a4ece3209cc999bee888949735866b8b9d9d160f
Sean Upton <sdupton@gmail.com>
2013-10-04 15:20:49 -0600

    some build.sh improvements, still in progress (currently may not work building python)

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@
 #       1. Python
 #           (based on collective.buildout.python)
 #       
-#       2. Plone 4.2 Application deployment build with:
-#           * Two ZServer instances
+#       2. Plone 4.3 Application deployment build with:
+#           * Three ZServer instances
 #           * A debug instance
 #           * RelStorage with PostgreSQL backend
 #           * memcached, libmemcached, pylibmc for RelStorage
@@ -14,8 +14,7 @@
 #           * HAProxy
 #           * Psycopg2, lxml, and other Python libraries
 #           * Test runners and coverage
-#           * Dexterity
-#           * supervisor to controll all your long-running processes
+#           * supervisor to control all your long-running processes
 #
 #       Assumption: this buildout takes care of everything but the front-end
 #                   web server for you; proxy from nginx|apache provided
@@ -26,7 +25,7 @@
 #                   * Can and should be extended with a site.cfg; see 
 #                     app/src/build/site.cfg.example
 #
-#                   * This buildout is run and tested on 32bit and 64bit
+#                   * This buildout is run and tested on 64bit
 #                     Linux and Mac OS X.
 #
 #   Default is deployment buildout, for a tiny buildout, pass single
@@ -46,18 +45,21 @@ APP_PYTHON="$BUILD_ROOT/python/python-$PYVER/bin/python"
 # buildout for Application Python, libraries; bootstrapped via system Python
 echo "=== BUILDING PYTHON ENVIRONMENT AND LIBRARIES ==="
 echo "    System Python: $SYSTEM_PYTHON"
-cp $BUILT_ROOT/pysite_in.cfg $BUILD_ROOT/python/site.cfg
+cp $BUILD_ROOT/pysite_in.cfg $BUILD_ROOT/python/site.cfg
 cd $BUILD_ROOT/python
-$SYSTEM_PYTHON bootstrap.py --distribute
-bin/buildout -N
+$SYSTEM_PYTHON bootstrap.py
+bin/buildout -N -c site.cfg
 
 # buildout for application server / hosting stack
 echo "=== BUILDING APPLICATION SERVER STACK BUILDOUT ==="
 cd $BUILD_ROOT/app
-$APP_PYTHON bootstrap.py --distribute
+$APP_PYTHON bootstrap.py
 if [ "$1" == "--tiny" ]; then
     echo "=== TINY/DEVELOPMENT-ONLY BUILDOUT REQUESTED ==="
     bin/buildout -N -c tiny.cfg
+elif [ "$1" == "--upiq" ]; then
+    echo "=== UPIQ BUILDOUT REQUESTED ==="
+    bin/buildout -N -c upiq.cfg
 else
     echo "=== DEFAULT (production) BUILDOUT REQUESTED ==="
     bin/buildout -N -c buildout.cfg
