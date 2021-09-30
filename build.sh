156686ffbec5cc1a7b3cced4b9481767859995c8
Sean Upton <sdupton@gmail.com>
2021-09-30 07:58:53 -0600

    build.sh uses virtualenv with requirements.txt
    Buildout is installed in the virtualenv.

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-#   build.sh: wrap buildouts contained for:
+#   build.sh: build environment with virtualenv (Python), buildout (app)
 #       
-#       1. Python
-#           (based on collective.buildout.python)
+#       1. Python: use virtualenv with requirements.txt
+#           - THIS SCRIPT BUILDS YOU A virtualenv called 'python27' in this dir
+#           - recommended: install pyenv (e.g. from homebrew),
+#           - then install Python 2.7.18 with pyenv
+#           - then ensure you pyenv has virtualenv, pip
 #       
 #       2. Plone 4.3 Application deployment build with:
-#           * Three ZServer instances
+#           * 4 ZServer instances
 #           * A debug instance
 #           * RelStorage with PostgreSQL backend
 #           * memcached, libmemcached, pylibmc for RelStorage
@@ -14,7 +17,7 @@
 #           * HAProxy
 #           * Psycopg2, lxml, and other Python libraries
 #           * Test runners and coverage
-#           * supervisor to control all your long-running processes
+#           * supervisor to control all long-running processes
 #
 #       Assumption: this buildout takes care of everything but the front-end
 #                   web server for you; proxy from nginx|apache provided
@@ -27,66 +30,31 @@
 #
 #                   * This buildout is run and tested on 64bit
 #                     Linux and Mac OS X.
-#
-#   Default is deployment buildout, for a tiny buildout, pass single
-#   option --tiny
-
-# some global constants
-PYVER=2.7
-UNAME=$(uname -a)
-SSLPATH=/usr/include/openssl
-
-# El Capitan requies this, earlier OS X can support it:
-if [[ $UNAME == *"Darwin"* ]]
-then
-    echo "Mac OS X detected, assuming we use homebrew OpenSSL..."
-    SSLPATH=/usr/local/opt/openssl/include/openssl
-    if [ ! -f $SSLPATH/ssl.h ]
-    then
-        echo "...Homebrew OpenSSL does not appear to be installed; exiting."
-        exit 1
-    fi
-fi
 
 # some paths necessary for execution
 BUILD_ROOT=$(dirname $0)
 if [ "$BUILD_ROOT" == "." ]; then
     BUILD_ROOT=$(pwd)
 fi
-SYSTEM_PYTHON=$(which python)
-APP_PYTHON="$BUILD_ROOT/python/python-$PYVER/bin/python"
-
-# force python rebuild by removing python/.installed.cfg
-if [ -f $BUILD_ROOT/python/.installed.cfg ]
-then
-    echo "Removing existing Python build configuration, in order to rebuild."
-    rm $BUILD_ROOT/python/.installed.cfg
-    rm -rf $BUILD_ROOT/python/parts/*
-fi
+APP_ENV=$BUILD_ROOT/python27
+APP_PYTHON="$APP_ENV/bin/python"
 
 # buildout for Application Python, libraries; bootstrapped via system Python
-echo "=== BUILDING PYTHON ENVIRONMENT AND LIBRARIES ==="
-echo "    System Python: $SYSTEM_PYTHON"
-cp $BUILD_ROOT/pysite_in.cfg $BUILD_ROOT/python/site.cfg
-cd $BUILD_ROOT/python
-$SYSTEM_PYTHON bootstrap.py
-bin/buildout -c site.cfg
+echo "=== CREATING PYTHON VIRTUAL ENVIRONMENT, INSTALLING LIBS FROM PYPI  ==="
+# pyenv or other python makes virtualenv
+python2.7 -m virtualenv python27
+# virtualenv python installs requirements in virtualenv:
+$APP_PYTHON -m pip install -r requirements.txt
 
 # buildout for application server / hosting stack
 echo "=== BUILDING APPLICATION SERVER STACK BUILDOUT ==="
 cd $BUILD_ROOT/app
-$APP_PYTHON bootstrap.py
-if [ "$1" == "--tiny" ]; then
-    echo "=== TINY/DEVELOPMENT-ONLY BUILDOUT REQUESTED ==="
-    bin/buildout -N -c tiny.cfg
-elif [ "$1" == "--app" ]; then
-    echo "=== FULL Shrubfrog APP BUILDOUT REQUESTED ==="
-    bin/buildout -N -c app.cfg
+
+if [ -f site.cfg ]; then
+    $APP_ENV/bin/buildout -N -c site.cfg
 else
-    echo "=== DEFAULT (production) BUILDOUT REQUESTED ==="
-    bin/buildout -N -c buildout.cfg
+    $APP_ENV/bin/buildout -N -c app.cfg
 fi
 
 echo "=== DONE WITH BUILDOUTS. ==="
 exit 0
-
